fc9b4e54ad58d1c68617cf6e22ff0a78f72808fe
raquelpz <rpz.ra.chel@gmail.com>
2025-05-20 13:08:40 +0200

    bucles innerHTML arrays

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ const btnReset = document.querySelector(".btn_reset");
 const inputAnime = document.querySelector(".input_anime");
 const album = document.querySelector(".album");
 let card;
-const cardFav = document.querySelector(".card_fav");
+// const cardFav = document.querySelector(".card_fav");
 
 const data = [
     {
@@ -28,19 +28,80 @@ const data = [
     title: "Naruto",
     title_english: "Naruto",
     title_japanese: "ナルト",
-    }];
+    },
+{
+    mal_id: 21,
+    url: "https://myanimelist.net/anime/20/Naruto",
+    images: {
+        jpg: {
+        image_url: "https://cdn.myanimelist.net/images/anime/1141/142503.jpg",
+        small_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503t.jpg",
+        large_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503l.jpg"
+        },
+        webp: {
+        image_url: "https://cdn.myanimelist.net/images/anime/1141/142503.webp",
+        small_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503t.webp",
+        large_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503l.webp"
+        }
+    },
+    title: "Naruto 3",
+    title_english: "Naruto",
+    title_japanese: "ナルト",
+    },
+        {
+    mal_id: 20,
+    url: "https://myanimelist.net/anime/20/Naruto",
+    images: {
+        jpg: {
+        image_url: "https://cdn.myanimelist.net/images/anime/1141/142503.jpg",
+        small_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503t.jpg",
+        large_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503l.jpg"
+        },
+        webp: {
+        image_url: "https://cdn.myanimelist.net/images/anime/1141/142503.webp",
+        small_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503t.webp",
+        large_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503l.webp"
+        }
+    },
+    title: "Naruto",
+    title_english: "Naruto",
+    title_japanese: "ナルト",
+    },
+{
+    mal_id: 21,
+    url: "https://myanimelist.net/anime/20/Naruto",
+    images: {
+        jpg: {
+        image_url: "https://cdn.myanimelist.net/images/anime/1141/142503.jpg",
+        small_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503t.jpg",
+        large_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503l.jpg"
+        },
+        webp: {
+        image_url: "https://cdn.myanimelist.net/images/anime/1141/142503.webp",
+        small_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503t.webp",
+        large_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503l.webp"
+        }
+    },
+    title: "Naruto 4",
+    title_english: "Naruto",
+    title_japanese: "ナルト",
+    }
+];
 
 // FUNCTION
 function printCard(){
-    
-    album.innerHTML = `
-        <ul class="card">
-            <div class="name">${data[0].title}</div>
-            <div class="img">
-                <img src="${data[0].images.webp.image_url}"
-                alt="Foto del anime: ${data[0].title}">
-            </div>
-        </ul>`;
+    let cards = " ";
+    for(let i = 0; i < data.length; i++){
+        cards += `
+            <ul class="card">
+                <div class="name">${data[i].title}</div>
+                <div class="img">
+                    <img src="${data[i].images.webp.image_url}"
+                    alt="Foto del anime: ${data[i].title}">
+                </div>
+            </ul>`;            
+        }    
+    album.innerHTML = cards;
 };
 
 // EVENT
@@ -59,9 +120,17 @@ btnReset.addEventListener ("click", () =>{
     inputAnime.value = " ";
 });
 
-album.addEventListener ("click", () =>{
-    console.log("Clickaste el cromo");
-    card.classList.toggle("card_fav");
+// card.addEventListener ("click", () =>{
+//     console.log("Clickaste el cromo");
+//     card.classList.toggle("card_fav");
+// });
+
+album.addEventListener ("click", (event) =>{
+    console.log(event.target);
+    if (event.target.classList.contains("card")){
+        console.log("Clickaste el cromo");
+        card.classList.toggle("card_fav");
+    }
 });
 
 // PROBANDO MOVIDAS
