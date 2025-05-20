ef3ec7ca17386977f6e66032ed81dbf05be3333b
raquelpz <rpz.ra.chel@gmail.com>
2025-05-20 11:46:07 +0200

    before the bucle

--- src/js/main.js
+++ src/js/main.js
@@ -6,37 +6,39 @@ const btnSearch = document.querySelector(".btn_buscar");
 const btnReset = document.querySelector(".btn_reset");
 const inputAnime = document.querySelector(".input_anime");
 const album = document.querySelector(".album");
-let card = document.querySelector(".card");
+let card;
 const cardFav = document.querySelector(".card_fav");
 
-const data = {
-mal_id: 20,
-url: "https://myanimelist.net/anime/20/Naruto",
-images: {
-jpg: {
-image_url: "https://cdn.myanimelist.net/images/anime/1141/142503.jpg",
-small_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503t.jpg",
-large_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503l.jpg"
-},
-webp: {
-image_url: "https://cdn.myanimelist.net/images/anime/1141/142503.webp",
-small_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503t.webp",
-large_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503l.webp"
-}
-},
-title: "Naruto",
-title_english: "Naruto",
-title_japanese: "ナルト",
-};
+const data = [
+    {
+    mal_id: 20,
+    url: "https://myanimelist.net/anime/20/Naruto",
+    images: {
+        jpg: {
+        image_url: "https://cdn.myanimelist.net/images/anime/1141/142503.jpg",
+        small_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503t.jpg",
+        large_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503l.jpg"
+        },
+        webp: {
+        image_url: "https://cdn.myanimelist.net/images/anime/1141/142503.webp",
+        small_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503t.webp",
+        large_image_url: "https://cdn.myanimelist.net/images/anime/1141/142503l.webp"
+        }
+    },
+    title: "Naruto",
+    title_english: "Naruto",
+    title_japanese: "ナルト",
+    }];
 
 // FUNCTION
 function printCard(){
+    
     album.innerHTML = `
         <ul class="card">
-            <div class="name">${data.title}</div>
+            <div class="name">${data[0].title}</div>
             <div class="img">
-                <img src="${data.images.webp.image_url}"
-                alt="Foto del anime: ${data.title}">
+                <img src="${data[0].images.webp.image_url}"
+                alt="Foto del anime: ${data[0].title}">
             </div>
         </ul>`;
 };
@@ -48,6 +50,7 @@ btnSearch.addEventListener ("click", () =>{
     console.log("Anime buscado: " + searchAnime);
     album.innerHTML = searchAnime;
     printCard();
+    card = document.querySelector(".card");
 });
 
 btnReset.addEventListener ("click", () =>{
@@ -58,7 +61,7 @@ btnReset.addEventListener ("click", () =>{
 
 album.addEventListener ("click", () =>{
     console.log("Clickaste el cromo");
-    album.classList.toggle("card_fav");
+    card.classList.toggle("card_fav");
 });
 
 // PROBANDO MOVIDAS
@@ -67,6 +70,8 @@ function renderAnime() {
 }
 
 fetch('https://api.jikan.moe/v4/anime?q=naruto')
-    .then()
-
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    });
 renderAnime();
